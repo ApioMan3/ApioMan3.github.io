600f1e0cc95183d5574de52c808890bebf3e220b
ApioMan3 <141684285+ApioMan3@users.noreply.github.com>
2024-03-05 20:06:31 -0300

    Prueba

--- js/contact.js
+++ js/contact.js
@@ -13,9 +13,12 @@ function validar(){
         if(!patronName.test(nombre)||!patronName.test(apellido)) throw "Los datos en el nombre/apellido no son correctos";
 
         if(!patronCorreo.test(correo)) throw "Correo no válido";
+
+        if(!comment) throw "Déjanos un comentario!!"
     }catch(err){
         mensaje.innerHTML = err;
         return false;
     }
+    localStorage.setItem("contacto",nombre);
     return true;
 }
